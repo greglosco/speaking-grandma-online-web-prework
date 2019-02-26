d3eaac1fbf16f65bf12974a6d37760e0029eb1bc
Gregory Losco <gtlosco@gmail.com>
2019-02-26 02:41:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  puts "I LOVE YOU TOO PUMPKIN"
+  if phrase == "I LOVE YOU GRANDMA"
+    "I LOVE YOU TOO PUMPKIN"
+  else "HUH?! SPEAK UP, SONNY!"
 end
   
